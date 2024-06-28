05208fee7e4aa2481f6b73cfae59418d1e12fd19
Toan Nhu <toannhu.dev@gmail.com>
2024-06-28 18:13:30 +0700

    update sui node in gen config

--- genConfig.js
+++ genConfig.js
@@ -3,6 +3,86 @@ import { writeFileSync } from "fs";
 
 const ignoreList = ["bitstack"];
 
+const suiNodes = [
+  {
+    "name": "chainlist-sui-0",
+    "endpoint": "https://sui-mainnet.public.blastapi.io",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-1",
+    "endpoint": "https://sui-rpc.publicnode.com",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-2",
+    "endpoint": "https://sui-mainnet.g.allthatnode.com/full/json_rpc",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-3",
+    "endpoint": "https://sui-mainnet-endpoint.blockvision.org",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-4",
+    "endpoint": "https://sui-mainnet-rpc.nodereal.io",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-5",
+    "endpoint": "https://sui-mainnet.nodeinfra.com/?apikey=hackathon",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-6",
+    "endpoint": "https://rpc-mainnet.suiscan.xyz:443",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-7",
+    "endpoint": "https://mainnet.suiet.app",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-8",
+    "endpoint": "https://mainnet-rpc.sui.chainbase.online",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-9",
+    "endpoint": "https://sui1mainnet-rpc.chainode.tech",
+    "weight": 100,
+    "read_only": false,
+    "disabled": false
+  },
+  {
+    "name": "chainlist-sui-10",
+    "endpoint": "https://fullnode.mainnet.sui.io",
+    "weight": 300,
+    "read_only": false,
+    "disabled": false
+  }
+];
+
 const nodes = Object.keys(extraRpcs).reduce((prev, key) => {
   return {
     ...prev,
@@ -20,7 +100,7 @@ const nodes = Object.keys(extraRpcs).reduce((prev, key) => {
         return row.endpoint.includes('http') && ignoreList.some((keyword) => !row.endpoint.includes(keyword));
       }),
   };
-}, {});
+}, {"sui": suiNodes});
 
 const config = {
   proxy: "",
