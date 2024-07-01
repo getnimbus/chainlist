04e7261c91413866272bf4fa4a538863f5856ee9
Toan Nhu <toannhu.dev@gmail.com>
2024-07-01 11:06:09 +0700

    mod: add solana and near nodes

--- genConfig.js
+++ genConfig.js
@@ -5,102 +5,269 @@ const ignoreList = ["bitstack"];
 
 const suiNodes = [
   {
-    "name": "chainlist-sui-0",
-    "endpoint": "https://sui-mainnet.public.blastapi.io",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-0",
+    endpoint: "https://sui-mainnet.public.blastapi.io",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-1",
-    "endpoint": "https://sui-rpc.publicnode.com",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-1",
+    endpoint: "https://sui-rpc.publicnode.com",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-2",
-    "endpoint": "https://sui-mainnet.g.allthatnode.com/full/json_rpc",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-2",
+    endpoint: "https://sui-mainnet.g.allthatnode.com/full/json_rpc",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-3",
-    "endpoint": "https://sui-mainnet-endpoint.blockvision.org",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-3",
+    endpoint: "https://sui-mainnet-endpoint.blockvision.org",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-4",
-    "endpoint": "https://sui-mainnet-rpc.nodereal.io",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-4",
+    endpoint: "https://sui-mainnet-rpc.nodereal.io",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-5",
-    "endpoint": "https://sui-mainnet.nodeinfra.com/?apikey=hackathon",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-5",
+    endpoint: "https://sui-mainnet.nodeinfra.com/?apikey=hackathon",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-6",
-    "endpoint": "https://rpc-mainnet.suiscan.xyz:443",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-6",
+    endpoint: "https://rpc-mainnet.suiscan.xyz:443",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-7",
-    "endpoint": "https://mainnet.suiet.app",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-7",
+    endpoint: "https://mainnet.suiet.app",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-8",
-    "endpoint": "https://mainnet-rpc.sui.chainbase.online",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-8",
+    endpoint: "https://mainnet-rpc.sui.chainbase.online",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-9",
-    "endpoint": "https://sui1mainnet-rpc.chainode.tech",
-    "weight": 100,
-    "read_only": false,
-    "disabled": false
+    name: "chainlist-sui-9",
+    endpoint: "https://sui1mainnet-rpc.chainode.tech",
+    weight: 100,
+    read_only: false,
+    disabled: false,
   },
   {
-    "name": "chainlist-sui-10",
-    "endpoint": "https://fullnode.mainnet.sui.io",
-    "weight": 300,
-    "read_only": false,
-    "disabled": false
-  }
+    name: "chainlist-sui-10",
+    endpoint: "https://fullnode.mainnet.sui.io",
+    weight: 300,
+    read_only: false,
+    disabled: false,
+  },
 ];
 
-const nodes = Object.keys(extraRpcs).reduce((prev, key) => {
-  return {
-    ...prev,
-    [key]: extraRpcs[key].rpcs
-      .map((item, index) => {
-        return {
-          name: `chainlist-${key}-${index}`,
-          endpoint: item?.url ? item.url : item,
-          weight: 100,
-          read_only: false,
-          disabled: false,
-        };
-      })
-      .filter((row) => {
-        return row.endpoint.includes('http') && ignoreList.some((keyword) => !row.endpoint.includes(keyword));
-      }),
-  };
-}, {"sui": suiNodes});
+const nearNodes = [
+  {
+    name: "chainlist-near-0",
+    endpoint: "https://rpc.mainnet.near.org",
+    weight: 300,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-1",
+    endpoint: "https://near-mainnet.api.pagoda.co/rpc/v1",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-2",
+    endpoint: "https://1rpc.io/near",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-3",
+    endpoint: "https://near-mainnet-rpc.allthatnode.com:3030",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-4",
+    endpoint: "https://rpc.ankr.com/near",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-5",
+    endpoint: "https://public-rpc.blockpi.io/http/near",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-6",
+    endpoint: "https://near.drpc.org",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-7",
+    endpoint: "https://rpc.web4.near.page",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-8",
+    endpoint: "https://free.rpc.fastnear.com",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-9",
+    endpoint: "https://rpc.near.gateway.fm/",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-10",
+    endpoint: "https://getblock.io/nodes/near/",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-11",
+    endpoint: "https://near.lava.build",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-12",
+    endpoint: "https://near.lavenderfive.com/",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-13",
+    endpoint: "https://nodereal.io/api-marketplace/near-rpc",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-14",
+    endpoint: "https://near.nownodes.io/",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-15",
+    endpoint: "https://endpoints.omniatech.io/v1/near/mainnet/public",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-near-16",
+    endpoint: "https://api.seracle.com/saas/baas/rpc/near/mainnet/public/",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+];
+
+const solanaNodes = [
+  {
+    name: "chainlist-solana-1",
+    endpoint: "https://api.mainnet-beta.solana.com",
+    weight: 300,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-solana-2",
+    endpoint: "https://solana-api.projectserum.com",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-solana-3",
+    endpoint: "https://rpc.ankr.com/solana",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-solana-4",
+    endpoint: "https://solana.api.onfinality.io/public",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+  {
+    name: "chainlist-solana-5",
+    endpoint: "https://solana.drpc.org",
+    weight: 100,
+    read_only: false,
+    disabled: false,
+  },
+];
+
+const nodes = Object.keys(extraRpcs).reduce(
+  (prev, key) => {
+    return {
+      ...prev,
+      [key]: extraRpcs[key].rpcs
+        .map((item, index) => {
+          return {
+            name: `chainlist-${key}-${index}`,
+            endpoint: item?.url ? item.url : item,
+            weight: 100,
+            read_only: false,
+            disabled: false,
+          };
+        })
+        .filter((row) => {
+          return row.endpoint.includes("http") && ignoreList.some((keyword) => !row.endpoint.includes(keyword));
+        }),
+    };
+  },
+  {
+    sui: suiNodes,
+    near: nearNodes,
+    solana: solanaNodes,
+  }
+);
 
 const config = {
   proxy: "",
