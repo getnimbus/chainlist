d000a76dfc7484861710a6a8b26cf27e939acb49
Toan Nhu <toannhu.dev@gmail.com>
2024-10-08 21:40:43 +0700

    Update genConfig.js

--- genConfig.js
+++ genConfig.js
@@ -41,13 +41,13 @@ const suiNodes = [
     read_only: false,
     disabled: false,
   },
-  {
-    name: "chainlist-sui-3",
-    endpoint: "https://sui-mainnet-rpc.nodereal.io",
-    weight: 100,
-    read_only: false,
-    disabled: false,
-  },
+  // {
+  //   name: "chainlist-sui-3",
+  //   endpoint: "https://sui-mainnet-rpc.nodereal.io",
+  //   weight: 100,
+  //   read_only: false,
+  //   disabled: false,
+  // },
   {
     name: "chainlist-sui-4",
     endpoint: "https://rpc-mainnet.suiscan.xyz:443",
